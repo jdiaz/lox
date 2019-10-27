e4f98a33974ed6bd7b35a68d8cb80b232d28302c
Jose Diaz <jrd.palacios@gmail.com>
2019-10-27 16:47:19 -0500

    Ast Printer

---
+++ src/AstPrinter.js
@@ -0,0 +1,57 @@
+const {log, level, logError} = require('./log')
+const Expr = require('./Expr')
+const Token = require('./Token')
+const TokenType = require('./TokenType')
+
+// Creates an unambiguous, if ugly, string representation of AST nodes.
+class AstPrinter {
+	print(expr) {
+		return expr.accept(this)
+	}
+
+  visitBinaryExpr(expr) {                  
+    return this.parenthesize(expr.operator.lexeme, [expr.left, expr.right])
+  }
+
+  visitGroupingExpr(expr) {
+    return this.parenthesize("group", [expr.expression])
+  }                  
+  
+  visitLiteralExpr(expr) {                
+    if (expr.value == null)
+    	return 'nil'
+    return expr.value
+  }                                                                
+  
+  visitUnaryExpr(expr) {                    
+    return this.parenthesize(expr.operator.lexeme, [expr.right])
+  }
+
+ 	parenthesize(name, exprs) {
+    const strBuilder = []
+    strBuilder.push(`(${name}`)
+    exprs.forEach(expr => {                              
+      strBuilder.push(' ')
+      strBuilder.push(expr.accept(this))
+    })                                
+    strBuilder.push(')')
+    return strBuilder.join('')
+ 	}
+}
+
+function main() {
+	expr = new Expr.Binary(
+		new Expr.Unary(
+			new Token(TokenType.MINUS, '-', null, 1),
+			new Expr.Literal(123),
+		),
+		new Token(TokenType.STAR, '*', null, 1),
+		new Expr.Grouping(
+			new Expr.Literal(47.67)
+		)
+	)
+
+	console.log(new AstPrinter().print(expr))
+}
+
+main()
