9d3ca0f60de0ac92aa8f3e1baa2287759fa3ae7b
Jose Diaz <jrd.palacios@gmail.com>
2020-03-04 22:47:47 -0600

    Convert tab to spaces size 2

--- src/Parser.js
+++ src/Parser.js
@@ -21,12 +21,12 @@ class ParseError extends Error {}
 
 class Parser {
 
-	constructor(tokens) {
-		this.tokens = tokens
-		this.current = 0
-	}
+  constructor(tokens) {
+    this.tokens = tokens
+    this.current = 0
+  }
 
-	parse() {       
+  parse() {       
     try {                       
       return this._expression()     
     } catch (parseError) {
@@ -34,105 +34,105 @@ class Parser {
     }
   }     
 
-	_expression()/*: Expr */{
-		return this._equality()
-	}
+  _expression()/*: Expr */{
+    return this._equality()
+  }
 
-	_equality() {
-		let expr = this._comparison()
+  _equality() {
+    let expr = this._comparison()
 
-		while (this._match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
-			const operator = this._previous()
-			const rightExpr = this._comparison()
-			expr = new Binary(expr, operator, rightExpr)
-		}
+    while (this._match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      const operator = this._previous()
+      const rightExpr = this._comparison()
+      expr = new Binary(expr, operator, rightExpr)
+    }
 
-		return expr
-	}
+    return expr
+  }
 
-	_comparison() {
-		let expr = this._addition()
+  _comparison() {
+    let expr = this._addition()
 
-		while (this._match(
-			TokenType.GREATER,
-			TokenType.GREATER_EQUAL,
-			TokenType.LESS,
-			TokenType.LESS_EQUAL
-		)) {
-			const operator = this._previous()
-			const rightExpr = this._addition()
-			expr = new Binary(expr, operator, rightExpr)
-		}
+    while (this._match(
+      TokenType.GREATER,
+      TokenType.GREATER_EQUAL,
+      TokenType.LESS,
+      TokenType.LESS_EQUAL
+    )) {
+      const operator = this._previous()
+      const rightExpr = this._addition()
+      expr = new Binary(expr, operator, rightExpr)
+    }
 
-		return expr
-	}
+    return expr
+  }
 
-	_addition() {
-		let expr = this._multiplication()
+  _addition() {
+    let expr = this._multiplication()
 
-		while (this._match(TokenType.MINUS, TokenType.PLUS)) {
-			const operator = this._previous()
-			const rightExpr = this._multiplication()
-			expr = new Binary(expr, operator, rightExpr)
-		}
+    while (this._match(TokenType.MINUS, TokenType.PLUS)) {
+      const operator = this._previous()
+      const rightExpr = this._multiplication()
+      expr = new Binary(expr, operator, rightExpr)
+    }
 
-		return expr
-	}
+    return expr
+  }
 
-	_multiplication() {
-		let expr = this._unirary()
+  _multiplication() {
+    let expr = this._unirary()
 
-		while (this._match(TokenType.SLASH, TokenType.STAR)) {
-			const operator = this._previous()
-			const rightExpr = this._unirary()
-			expr = new Binary(expr, operator, rightExpr)
-		}
+    while (this._match(TokenType.SLASH, TokenType.STAR)) {
+      const operator = this._previous()
+      const rightExpr = this._unirary()
+      expr = new Binary(expr, operator, rightExpr)
+    }
 
-		return expr
-	}
+    return expr
+  }
 
-	_unirary() {
-		if (this._match(TokenType.BANG, TokenType.MINUS)) {
-			const operator = this._previous()
-			const right = this._unirary()
-			return new Unary(operator, right)
-		}
+  _unirary() {
+    if (this._match(TokenType.BANG, TokenType.MINUS)) {
+      const operator = this._previous()
+      const right = this._unirary()
+      return new Unary(operator, right)
+    }
 
-		return this._primary()
-	}
+    return this._primary()
+  }
 
-	_primary() {
-		if (this._match(TokenType.FALSE)) return new Literal(false);
-		if (this._match(TokenType.TRUE)) return new Literal(true);
-		if (this._match(TokenType.NIL)) return new Literal(null);
+  _primary() {
+    if (this._match(TokenType.FALSE)) return new Literal(false);
+    if (this._match(TokenType.TRUE)) return new Literal(true);
+    if (this._match(TokenType.NIL)) return new Literal(null);
 
-		if (this._match(TokenType.NUMBER, TokenType.STRING)) {
-			return new Literal(this._previous().literal)
-		}
+    if (this._match(TokenType.NUMBER, TokenType.STRING)) {
+      return new Literal(this._previous().literal)
+    }
 
-		if (this._match(TokenType.LEFT_PAREN)) {
-			const expr = this._expression()
-			this._consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
-			return new Grouping(expr)
-		}
+    if (this._match(TokenType.LEFT_PAREN)) {
+      const expr = this._expression()
+      this._consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+      return new Grouping(expr)
+    }
 
-		throw this._error(this._peek(), "Expected expression.")
-	}
+    throw this._error(this._peek(), "Expected expression.")
+  }
 
-	_consume(tokenType, message)/*Token*/{
-		if (this._check(tokenType))
-			return this._advance()
+  _consume(tokenType, message)/*Token*/{
+    if (this._check(tokenType))
+      return this._advance()
 
-		throw this._error(this._peek(), message)
-	}
+    throw this._error(this._peek(), message)
+  }
 
-	 _error(token, message)/*ParseError*/{
-	 	//console.log(message)
-		Lox.error(token, message)
-		return new ParseError()
-	}
+   _error(token, message)/*ParseError*/{
+    //console.log(message)
+    Lox.error(token, message)
+    return new ParseError()
+  }
 
-	synchronize() {                 
+  synchronize() {                 
     this._advance();
 
     while (!this._isAtEnd()) {                       
@@ -155,39 +155,39 @@ class Parser {
     }
   } 
 
-	_match(...tokenTypes) {
-		for (let i = 0; i < tokenTypes.length; i++) {
-			if (this._check(tokenTypes[i])) {
-				this._advance()
-				return true
-			}
-		}
-		return false
-	}
+  _match(...tokenTypes) {
+    for (let i = 0; i < tokenTypes.length; i++) {
+      if (this._check(tokenTypes[i])) {
+        this._advance()
+        return true
+      }
+    }
+    return false
+  }
 
-	_check(tokenType) {
-		if (this._isAtEnd())
-			return false
-		return this._peek().tokenType === tokenType
-	}
+  _check(tokenType) {
+    if (this._isAtEnd())
+      return false
+    return this._peek().tokenType === tokenType
+  }
 
-	_advance() {
-		if (!this._isAtEnd())
-			this.current++
-		return this._previous()
-	}
+  _advance() {
+    if (!this._isAtEnd())
+      this.current++
+    return this._previous()
+  }
 
-	_isAtEnd() {
-		return this._peek().tokenType === TokenType.EOF
-	}
+  _isAtEnd() {
+    return this._peek().tokenType === TokenType.EOF
+  }
 
-	_peek() {
-		return this.tokens[this.current]
-	}
+  _peek() {
+    return this.tokens[this.current]
+  }
 
-	_previous() {
-		return this.tokens[this.current - 1]
-	}
+  _previous() {
+    return this.tokens[this.current - 1]
+  }
 }
 
 module.exports = Parser
